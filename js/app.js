890d1c33c024903d18bbff1fe8af4a105f34c514
paperboatz <tiffwork25@gmail.com>
2017-03-20 17:39:09 -0400

    decimals & input $ and concatenating $

--- js/app.js
+++ js/app.js
@@ -1,38 +1,53 @@
 // ======== Step ==========
 
-
 // export module so it can be executed via command line with node
 // in terminal: node -e "require('./app').total(1299.99, 3, \"food\")"
 
+// Noticed there is a "$" preceding input in the example
+// so we will delete that in the function
+// terminal command now: node -e "require('./app').total(\"\$1299.99\", 3, \"food\")"
+
+// Make endOutput to two decimal points
+// Initially had a Math.ceil() to round up, but noticed that for "drugs" in example, the output is NOT rounded
+// instead just cut off to two decimals 
+
+// add a dollarsign to the endOutput
+
+// changed to else/if statment on typeTotal
+// I want to catch if they choose anything other, the type will still be caught 
+
 module.exports.total = function(input, numPersons, type){
 
-	var baseMarkup = input * 1.05;
-	var endTotal = '';
+  var total = '';
+  var endOutput = '';
 
-	function personTotal(numPersons, baseMarkup){
-	  var personPercent = numPersons * 0.012;
-	  var personTotal = baseMarkup * personPercent; 
+  var input = input.substring(1);
+  var baseMarkup = input * 1.05;
 
-	  return personTotal;
+  function personTotal(numPersons, baseMarkup){
+	var personPercent = numPersons * 0.012;
+	var personTotal = baseMarkup * personPercent; 
+
+	return personTotal;
+  }
+
+  function typeTotal(type, baseMarkup){
+	if (type === 'pharmaceuticals'){
+	  return baseMarkup * 0.075;
+	} else if (type === 'food') {
+      return baseMarkup * 0.13;
+	} else if (type === 'electronics'){
+	  return baseMarkup * 0.02;
+	} else {
+	  return 0;
 	}
+  }
 
-	function typeTotal(type, baseMarkup){
-	  if (type === 'pharmaceuticals'){
-		return baseMarkup * 0.075;
-	  }
-	  if (type === 'food'){
-		return baseMarkup * 0.13;
-	  }
-	  if (type === 'electronics'){
-		return baseMarkup * 0.02;
-	  }
-	  if (type === 'else') {
-		return baseMarkup;
-	  }
-    }
+  total = baseMarkup + personTotal(numPersons, baseMarkup) + typeTotal(type, baseMarkup);
+  endOutput = '$' + total.toFixed(2);
 
-  	endTotal = baseMarkup + personTotal(numPersons, baseMarkup) + typeTotal(type, baseMarkup);
-    console.log(endTotal); 
+  console.log(endOutput); 
 }
+
 
 
