48d414482c2e6e2664c800ee700289592d2de4e1
paperboatz <tiffwork25@gmail.com>
2017-03-20 13:17:40 -0400

    created function to get one output

--- js/app.js
+++ js/app.js
@@ -1,40 +1,44 @@
-// ======== Step 2 ==========
-// substitute hardcode with changable arguments
-// create simple functions 
+// ======== Step ==========
 
-// Debug in console.log to check why I am not getting the same as my hardcode
-// Noticed I was putting wrong argumetns in my function
+// Created function to wrap so a user can enter one command to get an ouptut
+// console.log to test 
 
-const input = 5432.00;
-let baseMarkup = input * 1.05;
-let output = '';
+// for typeTotal, I considered making if/else conditionals
+// the reason I chose to use if statements was because
+// in the example we were given "drugs" which could/could not fit into pharma type
+// in my opinion better in the UI, a user would be given set options
+// so they are forced to choose between these 4 categories
+// or else we would have to make a list of what each type words could fit under type category === confusion
 
-function personTotal(numPersons, baseMarkup){
-	let personPercent = numPersons * 0.012;
-	let personTotal = baseMarkup * personPercent; 
+function total(input, numPersons, type){
 
-	return personTotal;
+	var baseMarkup = input * 1.05;
+
+	function personTotal(numPersons, baseMarkup){
+	  var personPercent = numPersons * 0.012;
+	  var personTotal = baseMarkup * personPercent; 
+
+	  return personTotal;
+	}
+
+	function typeTotal(type, baseMarkup){
+	  if (type === 'pharmaceuticals'){
+		return baseMarkup * 0.075;
+	  }
+	  if (type === 'food'){
+		return baseMarkup * 0.13;
+	  }
+	  if (type === 'electronics'){
+		return baseMarkup * 0.02;
+	  }
+	  if (type === 'else') {
+		return baseMarkup;
+	  }
+    }
+
+    return baseMarkup + personTotal(numPersons, baseMarkup) + typeTotal(type, baseMarkup);
 }
 
-function typeTotal(type, baseMarkup){
-  if (type === 'pharmaceuticals'){
-	return baseMarkup * 0.075;
-  }
-  if (type === 'food'){
-	return baseMarkup * 0.13;
-  }
-  if (type === 'electronics'){
-	return baseMarkup * 0.02;
-  }
-  if (type === 'else') {
-	return baseMarkup;
-  }
-}
-
-output = baseMarkup + personTotal(1, baseMarkup) + typeTotal('pharmaceuticals', baseMarkup);
-
-console.log(output); // great it works 
-
-
+console.log(total(1299.99, 3, 'food'));
 
 
