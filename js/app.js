1db93be6185d0f6758af6724655cf193ef3e59ef
paperboatz <tiffwork25@gmail.com>
2017-03-20 18:03:25 -0400

    remove commas from input value and add commas back for output value

--- js/app.js
+++ js/app.js
@@ -1,20 +1,9 @@
 // ======== Step ==========
 
-// export module so it can be executed via command line with node
-// in terminal: node -e "require('./app').total(1299.99, 3, \"food\")"
+// terminal command: node -e "require('./app').total(\"\$1299.99\", 3, \"food\")"
+// We see user inputs dollar value with commas in the example?
+// we will apply that change in input and output 
 
-// Noticed there is a "$" preceding input in the example
-// so we will delete that in the function
-// terminal command now: node -e "require('./app').total(\"\$1299.99\", 3, \"food\")"
-
-// Make endOutput to two decimal points
-// Initially had a Math.ceil() to round up, but noticed that for "drugs" in example, the output is NOT rounded
-// instead just cut off to two decimals 
-
-// add a dollarsign to the endOutput
-
-// changed to else/if statment on typeTotal
-// I want to catch if they choose anything other, the type will still be caught 
 
 module.exports.total = function(input, numPersons, type){
 
@@ -22,12 +11,12 @@ module.exports.total = function(input, numPersons, type){
   var endOutput = '';
 
   var input = input.substring(1);
-  var baseMarkup = input * 1.05;
+  var inputStripped = parseFloat(input.replace(/,/g, ''))
+  var baseMarkup = inputStripped * 1.05;
 
   function personTotal(numPersons, baseMarkup){
 	var personPercent = numPersons * 0.012;
 	var personTotal = baseMarkup * personPercent; 
-
 	return personTotal;
   }
 
@@ -44,7 +33,9 @@ module.exports.total = function(input, numPersons, type){
   }
 
   total = baseMarkup + personTotal(numPersons, baseMarkup) + typeTotal(type, baseMarkup);
-  endOutput = '$' + total.toFixed(2);
+  var totalDec = total.toFixed(2);
+  totalCommas = totalDec.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  endOutput = '$' + totalCommas;
 
   console.log(endOutput); 
 }
